e5dabfd89bd409a19f82e73ec64687937942834b
cmeyer <claremeyer26@gmail.com>
2018-10-25 13:55:52 -0500

    got exercise7 to work

--- exercise7/DeletePost.php
+++ exercise7/DeletePost.php
@@ -7,9 +7,8 @@ delete posts that were checked in DeletePosts.html
  */
 
  $mysqli = new mysqli("mysql.eecs.ku.edu", "c111m575", "sqlPasswordClare", "c111m575");
- echo "hi";
  $deletePosts = $_POST['check_list'];
- /*$thePosts = mysqli_query($mysqli, "DELETE FROM Posts WHERE post_id='$deletePosts'");*/
+ $numberChecked = 0;
 
  if(isset($_POST['submit'])){
    if(!empty($_POST['check_list'])){
@@ -17,32 +16,23 @@ delete posts that were checked in DeletePosts.html
    }
  }
 
- echo "number checked: " . $numberChecked . '</br>';
+ echo "posts selected: " . $numberChecked . '</br>';
 
-/* for($i=0; $i<$numberChecked; $i++){
-   echo $deletePosts[$i];
- }*/
-/* foreach($_POST['check_list'] as $item)){*/
+ if($numberChecked != 0){
+   echo "Posts to be deleted: " . '</br>';
+   for($i=0; $i<$numberChecked; $i++){
+     $please = $deletePosts[$i];
+     $values = "SELECT * FROM Posts WHERE content='$please'";
+     $theQuery = mysqli_query($mysqli, $values);
+     while($row = mysqli_fetch_array($theQuery)){
+       $choose[] = $row['post_id'];
+      }
+      echo "Post ID: " . $choose[$i] . " Post content: " . $deletePosts[$i] . '</br>';
+      $deleteText = "DELETE FROM Posts WHERE post_id = '$choose[$i]'";
+      $deleteQuery = mysqli_query($mysqli, $deleteText);
+      echo "Selected posts removed from the database" . '</br>';
+   }
+ }
 
-if(is_array($_POST['check_list'])){
-  foreach($_POST['check_list'] as $value){
-    echo "in here";
-    echo $value;
-  }
-}
-
-if(isset($_POST['check_list'])){
-  echo "check1" . '</br>';
-  if(is_array($_POST['check_list'])){
-    echo "check2" . '</br>';
-    foreach($_POST['check_list'] as $value){
-      echo "check3" . $value . '</br>';
-      print_r($value['name'][0]['value']);
-    }
-  } else {
-    $value = $_POST['check_list'];
-    echo $value;
-  }
-}
   $mysqli->close($mysqli);
 ?>
